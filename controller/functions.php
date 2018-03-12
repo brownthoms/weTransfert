22a0159eea957e74e60a22021ed0ae1bd9291875
thomas <brownaformac@gmail.com>
2018-03-12 11:35:48 +0100

    function

--- controller/functions.php
+++ controller/functions.php
@@ -1,6 +1,22 @@
 <?php
-    include '../db/connectDB.php';
+    include '../modele/connectDB.php';
 
-    function recupIdNonLog(){
+    function recupNonLog($bdd){
+        $reponse = $bdd->query('SELECT * FROM nonlog');
+        while ($donnees = $reponse->fetch()) {
+            echo $donnees['id'];
+            echo $donnees['file'];
+        }
+
+    }
+    function recupLog($bdd){
+        $reponse = $bdd->query('SELECT * FROM `log`');
+        while ($donnees = $reponse->fetch()){
+            echo $donnees['id'];
+        }
+    }
+    function logIn(){
         
     }
+    recupNonLog($bdd);
+    recupLog($bdd);
