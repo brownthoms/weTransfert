f90a4402e127a3d54922a54c08298b4d92c9e245
thomas <brownaformac@gmail.com>
2018-03-13 16:03:56 +0100

    time

--- controller/upload.php
+++ controller/upload.php
@@ -68,7 +68,8 @@ function taille(){
         if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $target_file)) {
             $bdd = connectDB();
             $file = $_FILES['fichier']['name'];
-            $newFile = "INSERT INTO nonlog(file) VALUES ('$file')";
+            $heure = date("H:i");
+            $newFile = "INSERT INTO nonlog(file, heure) VALUES ('$file', '$heure')";
             $bdd->exec($newFile);
             echo "The file ". basename( $_FILES["fichier"]["name"]). " has been uploaded.";
             header("location: ../index.php");
@@ -77,3 +78,19 @@ function taille(){
             echo "Sorry, there was an error uploading your file.";
         }
     }
+
+function delete(){
+    $req = mysql_query("SELECT * FROM membres WHERE actif='0'"); //On récupère tous les membres pas actifs
+        while ($donnees = mysql_fetch_array($req))
+        {
+            $date_inscription = $donnees['ta_colonne_date_inscription'];
+            $date = time();
+            $difference = $date - $date_inscription;
+            $nbreSecondesEn2Jours = (3600*24)*2;
+            if ($difference > $nbreSecondesEn2Jours)//On supprime le membre
+            {
+            $id = $donnees['id'];
+            mysql_query("DELETE FROM membres WHERE id='$id'");
+            }
+        }
+}
