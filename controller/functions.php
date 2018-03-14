60c869c42f4bde85b68c4ea210157e0145a078d8
MikeMoleka <mike.moleka@gmail.com>
2018-03-14 12:09:09 +0100

    mise a jour

--- controller/functions.php
+++ controller/functions.php
@@ -3,20 +3,42 @@
 
     function verifMail($mail){
         $bdd = connectDB();
-        $reponse = $bdd->query("SELECT mail FROM user WHERE user.mail = '$mail'");
-        $row = $reponse->fetch();
-        $mail = $row['mail'];
-        return $mail;
-
+        try{
+            $reponse = $bdd->query("SELECT mail FROM user WHERE user.mail = '$mail'");
+            $row = $reponse->fetch();
+            $mail = $row['mail'];
+            return $mail;
+        }catch(Exception $e){
+            die("request error");
+        }
     }
 
     function verifPassword($passWord){
         $bdd = connectDB();
-        $reponse = $bdd->query("SELECT mdp FROM user WHERE user.mdp = '$passWord'");
-        $row = $reponse->fetch();
-        $mdp = $row['mdp'];
-        return $mdp;
+        try{
+            $reponse = $bdd->query("SELECT mdp FROM user WHERE user.mdp = '$passWord'");
+            $row = $reponse->fetch();
+            $mdp = $row['mdp'];
+            return $mdp;
+        }catch(Exception $e){
+            die("request error");
+        }
     }
+
+    function afficherFichier($mail){
+        $bdd = connectDB();
+        try{
+			$req = $bdd->query("SELECT fichier, mail
+								FROM `log`
+								INNER JOIN user ON user.id = user_id
+                                WHERE user.mail = '$mail'");
+            $reponse = $req->fetch();
+			return $reponse;
+		}catch (Exception $e) {
+			die("reque error");
+		}
+    }
+
 
     function recupNonLog($bdd){
         $reponse = $bdd->query('SELECT * FROM nonlog');
