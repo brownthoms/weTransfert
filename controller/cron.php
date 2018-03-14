1ac0291dd220692c07e850bc64183b2be30ec763
thomas <brownaformac@gmail.com>
2018-03-14 11:32:04 +0100

    recup

--- controller/cron.php
+++ controller/cron.php
@@ -7,7 +7,7 @@ $bdd = connectDB();
 // effacer les nom de fichiers dans la BDD nonLog = PUBLIC
 function deleteFileFromDB($bdd) {
     $stmt = $bdd->prepare("DELETE FROM nonlog
-    WHERE DATEDIFF(CURDATE(), date) < 7");
+    WHERE heure >= (CURTIME() - 10)");
 
     $stmt->execute();
 
@@ -16,7 +16,7 @@ function deleteFileFromDB($bdd) {
 // effacer les fichiers PUBLIC sur le disque
 function selectFileAndDeleteFile($bdd){
      $stmt = $bdd->prepare("SELECT heure FROM nonlog
-     WHERE DATEDIFF(CURDATE(), date) < 1");
+     WHERE CURTIME() - 10");
 
      $stmt->execute();
      $table = array();
@@ -37,5 +37,33 @@ function selectFileAndDeleteFile($bdd){
 
 // effacer les nom de fichiers dans la BDD
  deleteFileFromDB($bdd);
+
+ // effacer les nom de fichiers dans la BDD Log = User
+ function deleteFileFromDBLog($bdd) {
+     $stmt = $bdd->prepare("DELETE FROM `log`
+     WHERE DATEDIFF(CURDATE(), date) < 1");
+
+     $stmt->execute();
+
+ }
+
+ // effacer les fichiers PUBLIC sur le disque
+ function selectFileAndDeleteFileLog($bdd){
+      $stmt = $bdd->prepare("SELECT heure FROM nonlog
+      WHERE DATEDIFF(CURDATE(), date) < 1");
+
+      $stmt->execute();
+      $table = array();
+
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      $i = 0;
+
+      while($row = $stmt->fetch()) {
+          $table[$i] = "../files/public/".$row['file'];
+          unlink($table[$i]);
+          $i++;
+      }
+  }
+
 
  ?>
