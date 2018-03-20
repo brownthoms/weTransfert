0b72d32223aa4252b2fa4f7039484ec187986d49
formagrey <formagrey@gmail.com>
2018-03-20 15:00:17 +0100

    test

--- index.php
+++ index.php
@@ -27,13 +27,9 @@
                             while(false !== ($fichier = readdir($dossier))) {
                                 if($fichier != '.' && $fichier != '..' && $fichier != 'index.php') {
                                     $nb_fichier++; // On incrémente le compteur de 1
-<<<<<<< HEAD
 
                                     echo '<div><a class="nomfile" href="files/public/' . $fichier .'">' . $fichier . '</a> -<a href="files/public/' . $fichier . '"download="files/public/' . $fichier . '">Download</a></div>';
 
-=======
-                                    echo '<div class="nomfile" >' . $fichier . ' - <a href="files/public/' . $fichier . '">Open</a> or <a href="files/public/' . $fichier . '" download="files/public/' . $fichier . '">Download</a></div>';
->>>>>>> f5c90aa03c764897c46fa394118c94ac5e4ccf6a
 
                                 } // On ferme le if (qui permet de ne pas afficher index.php, etc.)
                             } // On termine la boucle
@@ -42,14 +38,10 @@
                             closedir($dossier);
                         }
                     ?>
-                    <div class="twitter">
-                        <a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical">Share</a>
-                    </div>
+
                 </div>
             </div>
         </div>
-        <script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
-        <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
         <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
